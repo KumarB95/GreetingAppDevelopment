152185831341b02c2c9db799ace8e32247894079
KumarB95 <kumarbalingal@gmail.com>
2021-12-14 16:42:03 +0530

    Created : Extended Service layers to get simple greeting message

--- src/main/java/com/bridgelabz/greetingappdevelopment/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingappdevelopment/service/GreetingService.java
@@ -4,8 +4,8 @@ import com.bridgelabz.greetingappdevelopment.model.Greeting;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.atomic.AtomicLong;
 
-@Service
-public class GreetingService implements IGreatingService{
+    @Service
+    public class GreetingService implements IGreatingService{
     private static final String template = "Hello world";
     private final AtomicLong counter = new AtomicLong();
 
